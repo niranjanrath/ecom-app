5192736b3e3212d6b711e07f279ddd1f2d210259
Niranjan <niranjan.rath@rabobank.nl>
2021-02-19 17:34:52 +0100

    changes for calculating chipping total, shipping cost and total value of the order

--- src/app/shopping-cart/item-list/item-list.component.ts
+++ src/app/shopping-cart/item-list/item-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnChanges, OnInit} from '@angular/core';
 import {CartService} from '../../services/cart.service';
 import {Product} from '../../models/product.model';
 
@@ -9,16 +9,25 @@ import {Product} from '../../models/product.model';
 })
 export class ItemListComponent implements OnInit {
   itemList: Product[] = [];
-
+  sumTotal = 0;
+  shippingTotal = 0;
+  total = 0;
   constructor(private cartService: CartService) {
 
   }
 
   ngOnInit(): void {
     this.itemList = this.cartService.shoppingItemList;
+    this.itemList.forEach(item => this.sumTotal += item.amount);
+    this.shippingTotal = this.sumTotal > 0 ? 6.95 : 0;
+    this.total = this.sumTotal + this.shippingTotal;
   }
 
   removeProductFromCart(product: Product): void {
+    this.sumTotal = 0;
     this.cartService.removeProductFromCart(product);
+    this.itemList.forEach(item => this.sumTotal += item.amount);
+    this.shippingTotal = this.sumTotal > 0 ? 6.95 : 0;
+    this.total = this.sumTotal + this.shippingTotal;
   }
 }
